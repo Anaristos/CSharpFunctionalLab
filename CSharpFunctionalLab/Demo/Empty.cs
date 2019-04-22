a811d440a07321be8bddaf38f327e75d070ae25b
Anaristos <anaristos@gmail.com>
2019-04-22 13:30:32 -0700

    Clean up code.

--- CSharpFunctionalLab/Demo/Empty.cs
+++ CSharpFunctionalLab/Demo/Empty.cs
@@ -4,14 +4,10 @@ namespace Demo
 {
     public class Empty : SpecificMoney
     {
-        public Empty(Currency currency) : base(currency)
-        {
-        }
+        public Empty(Currency currency) : base(currency) { }
 
-        public override Money On(Timestamp time) => 
-            this;
+        public override Money On(Timestamp time) => this;
 
-        public override Tuple<Amount, Money> Take(decimal amount) => 
-            Tuple.Create(Amount.Zero(base.Currency), (Money)this);
+        public override Tuple<Amount, Money> Take(decimal amount) => Tuple.Create(Amount.Zero(Currency), (Money)this);
     }
 }
